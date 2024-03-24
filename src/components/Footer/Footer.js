6a8dce8c01d37a0a336ec126b5581ed8631f7352
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-03-24 01:16:11 +0000

    Add files via upload
    footer js first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <section className="contact-info">
+          <p>Office 102, Manchester, M4 3EQ</p>
+          <p>+44 20 7123 4567</p>
+        </section>
+        <section className="copyright">
+          <p>&copy; James Hutchinson 2024</p>
+        </section>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
