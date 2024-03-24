eeb5bf8e1cca8166ce674414f579332e3a4f740b
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-03-24 01:17:27 +0000

    Add files via upload
    footer second commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './footer.css';
+import instagramIcon from '../../assets/instagramicon.png';
+import linkedinIcon from '../../assets/linkedinicon.png';
+import facebookIcon from '../../assets/facebookicon.png';
+import twitterIcon from '../../assets/twittericon.png';
 
 const Footer = () => {
   return (
     <>
       <footer className="footer">
+        <section className="social-media">
+          <a href="https://www.instagram.com">
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
+          <a href="https://www.linkedin.com">
+            <img src={linkedinIcon} alt="LinkedIn" />
+          </a>
+          <a href="https://www.facebook.com">
+            <img src={facebookIcon} alt="Facebook" />
+          </a>
+          <a href="https://twitter.com">
+            <img src={twitterIcon} alt="Twitter" />
+          </a>
+        </section>
         <section className="contact-info">
           <p>Office 102, Manchester, M4 3EQ</p>
           <p>+44 20 7123 4567</p>
