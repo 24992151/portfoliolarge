9ce4ba6bbb94a94ae2beb85689ffde9c5390850c
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-03-25 21:58:24 +0000

    Commit 1 Projects HTML
    Base Content

---
+++ src/pages/Projects/ProjectsPage.js
@@ -0,0 +1,22 @@
+// ProjectsPage.js
+import React from 'react';
+import './projectsPage.css'; // Import your CSS file for styling
+
+const ProjectsPage = () => {
+  return (
+    <section>
+      <section className="projects-title-bar">
+        <h1>Grab a glimpse of our work</h1>
+      </section>
+
+      <section className="content">
+        <section className="section1-content">
+          <h2 className="section-heading">Ours Projects</h2>
+          <p>Over the years, the team here at Dubai-Landscapes has worked on a whole host of projects throughout the UAE, helping to enhance the outdoor space of numerous clients in the region. While we could tell you about our best work, we thought it would be better to show you instead. On this page, you’ll find our projects gallery where you can browse through some of our recent projects.</p>
+        </section>
+        </section>
+      </section>
+  );
+}
+
+export default ProjectsPage;
