011fdc6320431f75e413914bd1d5690c21f22d05
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-03-25 00:05:51 +0000

    Commit 2 Homepage JS
    Section 1 - subheader

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -9,6 +9,13 @@ const HomePage = () => {
         <div className="overlay">
           <h1>UAE’s leading landscapers</h1>
         </div>
+
+        <section className="section1">
+          <h2 className="section-heading">Garden Landscaping</h2>
+          <p className="section-text">
+            Your garden or outdoor space is an extension of your entire home. A place where you can relax after a long day, entertain friends and family or socialise with loved ones. At Dubai Landscapes, we’ve spent a decade in the UAE, crafting bespoke outdoor living spaces that achieve exactly what our clients desire.
+          </p>
+        </section>
       </body>
     );
 };
