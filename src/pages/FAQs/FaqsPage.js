0f93d5ab47acf35d61f7daffc5a24b485bc077c5
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-03-25 23:03:25 +0000

    Commit 1 FAQs JS
    Primary Content

---
+++ src/pages/FAQs/FaqsPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './faqsPage.css'; // Import your CSS file for styling
+
+const FaqsPage = () => {
+  return (
+    <section>
+      <section className="faqs-title-bar">
+        <h1>Answering your questions</h1>
+      </section>
+
+      <section className="content">
+        <section className="section1-content">
+          <h2 className="section-heading">Frequently Asked Questions</h2>
+          <p>Want to know more about the services we offer? Maybe you have specific questions about our processes that you’d like to learn more about? Or perhaps you just need clarification ona few details. Whatever the case, the team at Dubai Landscapes is on hand to help answer any questions you have.Check out our list of frequently asked questions below; if your question is not covered feel free to contact our team directly. Drop us a message on WhatsApp or give us a call.</p>
+        </section>
+      </section>
+    </section>
+  );
+}
+
+export default FaqsPage;
