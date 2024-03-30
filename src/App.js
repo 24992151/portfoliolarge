b328e6e69e0b94de72c11df549a97f966a5222e8
James Hutchinson <114491250+24992151@users.noreply.github.com>
2024-03-30 17:30:21 +0000

    Commit 2 App.js Routes
    All Routes added & Importing

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomePage from './pages/Home/HomePage';
 import LandscapingPage from './pages/Landscaping/LandscapingPage';
-// import ProjectsPage from './pages/Projects/ProjectsPage';
-// import FaqsPage from './pages/FAQs/FaqsPage';
-// import ContactPage from './pages/Contact/ContactPage';
+import ProjectsPage from './pages/Projects/ProjectsPage';
+import FaqsPage from './pages/FAQs/FaqsPage';
+import ContactPage from './pages/Contact/ContactPage';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <Router>
-       <div className="layout">
-        <Header /> {/* Include Header component */}
-           <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/landscaping" element={<LandscapingPage/>} />
+      <div className="layout">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/landscaping" element={<LandscapingPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/faqs" element={<FaqsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>
-      <Footer/> {/* Include Header component */}
+      <Footer />
     </Router>
   );
 }
